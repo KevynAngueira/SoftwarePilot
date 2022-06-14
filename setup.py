bf2d8e62ac7365dbfefd2ec29bdf05627e35d231
boredbot2 <84028301+boredbot2@users.noreply.github.com>
2022-06-14 11:08:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,14 @@ setuptools.setup(
     name="ParrotController",
     version="1.0.0", # eg:1.0.0
     author="Kevyn Angueira Irizarry",
-    author_email="<YOUR EMAIL>",
+    author_email="kevyn.angueira@gmail.com",
     license="<YOUR_LICENSE>",
     description="Wrapper for Parrot Olympe",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/boredbot2/Parrot-Controller",
-    packages = ['<YOUR_PACKAGE_NAME>'],
-    # project_urls is optional
-    project_urls={
-        "Bug Tracker": "<BUG_TRACKER_URL>",
-    },
-    key_words="<KEY WORDS>",
+    packages = ['ParrotController'],
+    key_words="Parrot Anafi, Drone",
     install_requires=required,
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
