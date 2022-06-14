0048cacc6b38703b69a1b30a673b536b4aa17c70
boredbot2 <84028301+boredbot2@users.noreply.github.com>
2022-06-14 11:16:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 long_desc = open("README.md").read()
-required = [<Dependent Packages>] # Comma seperated dependent libraries name
+required = [csv, math, os, time, queue, shlex, subprocess, threading, cv2, re, requests, shutil, olympe] # Comma seperated dependent libraries name
 
 setuptools.setup(
     name="ParrotController",
-    version="1.0.0", # eg:1.0.0
+    version="1.0.0",
     author="Kevyn Angueira Irizarry",
     author_email="kevyn.angueira@gmail.com",
-    license="<YOUR_LICENSE>",
+    license="liscence.txt",
     description="Wrapper for Parrot Olympe",
     long_description=long_description,
     long_description_content_type="text/markdown",
