a2f546ea11ccbc26ba984353d590e68701ca43a2
boredbot2 <84028301+boredbot2@users.noreply.github.com>
2022-06-23 16:43:35 -0400

    Create __init__.py

---
+++ 1.0.2/__init__.py
@@ -0,0 +1,7 @@
+from .AnafiController import AnafiController
+from .AnafiCamera import AnafiCamera
+from .AnafiCameraMedia import AnafiCameraMedia
+from .AnafiCameraControls import AnafiCameraControls
+from .AnafiPiloting import AnafiPiloting
+from .AnafiRTH import AnafiRTH
+from .SoftwarePilot import SoftwarePilot
