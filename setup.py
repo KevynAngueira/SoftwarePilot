4c833850c621ab06a1a68469913ded270f1bc4a5
boredbot2 <84028301+boredbot2@users.noreply.github.com>
2022-06-23 16:40:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 import setuptools
 
 long_desc = open("README.md").read()
-required = [csv, math, os, time, queue, shlex, subprocess, threading, cv2, re, requests, shutil, olympe]
+required = ["cv2>=2.0.2", "requests>=2.27.1", "olympe>=0.0.1"]
 
 setuptools.setup(
-    name="ParrotController",
-    version="1.0.0",
+    name="ParrotAnafiController",
+    version="1.0.1",
     author="Kevyn Angueira Irizarry",
     author_email="kevyn.angueira@gmail.com",
     license="liscence.txt",
     description="Wrapper for Parrot Olympe",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/boredbot2/Parrot-Controller",
-    packages = ['ParrotController'],
-    key_words="Parrot Anafi, Drone",
+    key_words="Parrot Anafi, Drone, Olympe",
     install_requires=required,
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
