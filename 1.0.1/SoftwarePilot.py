8c07733d7aacd6800e457d95ebbbc5b450ac9c44
boredbot2 <84028301+boredbot2@users.noreply.github.com>
2022-07-18 19:11:46 -0500

    Delete 1.0.1 directory

--- 1.0.1/SoftwarePilot.py
+++
@@ -1,8 +0,0 @@
-from AnafiController import AnafiController
-
-class SoftwarePilot:
-	def __init__(self):
-		self.drone_dict = { "parrot_anafi": AnafiController }
-	
-	def initialize_drone(self, drone_type, connection_method, download_dir):	
-		return self.drone_dict[drone_type](connection_method, download_dir)
--- 1.0.1/__init__.py
+++
@@ -1,7 +0,0 @@
-from .AnafiController import AnafiController
-from .AnafiCamera import AnafiCamera
-from .AnafiCameraMedia import AnafiCameraMedia
-from .AnafiCameraControls import AnafiCameraControls
-from .AnafiPiloting import AnafiPiloting
-from .AnafiRTH import AnafiRTH
-from .SoftwarePilot import SoftwarePilot
