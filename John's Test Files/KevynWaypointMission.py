408421ee193e0f9e8321aa6d8bb23936d21cdd6a
boredbot2 <84028301+boredbot2@users.noreply.github.com>
2023-02-25 13:57:20 -0600

    Update KevynWaypointMission.py

--- John's Test Files/KevynWaypointMission.py
+++ John's Test Files/KevynWaypointMission.py
@@ -4,7 +4,7 @@ program written by Kevyn Angueira.
 
 '''
 
-from SoftwarePilot import SoftwarePilot
+# from SoftwarePilot import SoftwarePilot
 import sys
 import csv
 import time
@@ -20,28 +20,28 @@ class WaypointTraverser:
 		self.waypoint_num = 0
 
 	# actionType = -1
-	def noAction(drone, param):
+	def noAction(self, drone, param):
 	    pass
 
 	# actionType = 0
-	def pauseDrone(drone, param):
+	def pauseDrone(self, drone, param):
 	    time.sleep(param/1000.0)
 
 	# actionType = 1
-	def takePhoto(drone, param):
+	def takePhoto(self, drone, param):
 	    drone.camera.media.take_photo()
 	    drone.camera.media.download_last_media(name = (self.filename + "_" + self.waypoint_num))
 
 	# actionType = 5
-	def tiltGimbal(drone, param):
+	def tiltGimbal(self, drone, param):
 	    drone.camera.controls.reset_alignment_offset()
 	    drone.camera.controls.set_alignment_offset(0, param, 0)
 
-	def executeAction(drone, action, param):
-	    action_map = {-1: noAction, 0: pauseDrone, 1: takePhoto, 5: tiltGimbal}
+	def executeAction(self, drone, action, param):
+	    action_map = {-1: self.noAction, 0: self.pauseDrone, 1: self.takePhoto, 5: self.tiltGimbal}
 	    action_map[action](drone, param)
 
-	def traverseWaypointFile(filename, drone):   
+	def traverseWaypointFile(self, filename, drone):   
 		self.filename = filename[:-3]
 		with open(filename, 'r') as waypoint_file:
 			# Creating a csv reader for the waypoint file
@@ -50,49 +50,57 @@ class WaypointTraverser:
 
 			self.waypoint_num = 0
 			for waypoint in waypoint_reader:            
-				latitude = waypoint[0]
-				longitude = waypoint[1]
+				latitude = float(waypoint[0])
+				longitude = float(waypoint[1])
 				'''
 				Unsure if altitude is measured in meters or in feet
 				Use top one if its in feet
 				'''
 				# altitude = waypoint[2]
-				altitude = waypoint[2]/3.28084
-				heading = waypoint[3]
+				altitude = float(waypoint[2])/3.28084
+				heading = float(waypoint[3])
 
 				drone.piloting.move_to(latitude, longitude, altitude, heading=heading)
-
-				for action in range(8, 37, 2):
-					executeAction(drone, action, action+1, filename, waypoint_num)
+				'''
+				print(latitude)
+				print(longitude)
+				print(altitude)
+				print(heading)
+				'''
+	
+				for x in range(8, 37, 2):
+					action = int(waypoint[x])
+					self.executeAction(drone, action, action+1)
+					print(action)
 					if (action == -1):
 						break
 				self.waypoint_num += 1
 
 
 if __name__ == "__main__":
-    '''
-    Unsure if altitude is measured in meters or in feet
-    Use top one if its in feet
-    '''
-    traverser = WaypointTraverser()
-    
-    # CLEARANCE_ALTITUDE = 10
-    CLEARANCE_ALTITUDE = 3
+	'''
+	Unsure if altitude is measured in meters or in feet
+	Use top one if its in feet
+	'''
+	traverser = WaypointTraverser()
 
-    waypoint_files = getWaypointFiles()
+	# CLEARANCE_ALTITUDE = 10
+	CLEARANCE_ALTITUDE = 3
 
-    sp = SoftwarePilot()
-    drone = sp.setup_drone("parrot_anafi", 1, "None")
-    drone.connect()
+	waypoint_files = getWaypointFiles()
+	
+	sp = SoftwarePilot()
+	drone = sp.setup_drone("parrot_anafi", 1, "None")
+	drone.connect()
 
-    home = drone.get_drone_coordinates()
-    drone.camera.media.setup_photo()
-    drone.piloting.takeoff()
+	home = drone.get_drone_coordinates()
+	drone.camera.media.setup_photo()
+	drone.piloting.takeoff()
+	
+	for filename in waypoint_files:
+		traverser.traverseWaypointFile(filename, drone)
+	
+	drone.piloting.move_to(home[0], home[1], CLEARANCE_ALTITUDE)
+	drone.piloting.land()
 
-    for filename in waypoint_files:
-        traverser.traverseWaypointFile(filename, drone)
-
-    drone.piloting.move_to(home[0], home[1], CLEARANCE_ALTITUDE)
-    drone.piloting.land()
-    
-    drone.disconnect()
+	drone.disconnect()
